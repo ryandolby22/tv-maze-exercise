12261708f36c2842705a1c3f094372c3520bbb66
ryandolby22 <115246771+ryandolby22@users.noreply.github.com>
2022-12-04 21:59:06 -0500

    Add files via upload

---
+++ tvmaze.js
@@ -0,0 +1,117 @@
+"use strict";
+
+const MISSING_IMAGE_URL = "https://img.freepik.com/premium-photo/retro-television-with-technical-difficulties-warning_698953-10478.jpg?w=1380";
+const TVMAZE_API_URL = "http://api.tvmaze.com/";
+
+const $showsList = $("#showsList");
+const $episodesList = $("#episodesList");
+const $episodesArea = $("#episodesArea");
+const $searchForm = $("#searchForm");
+
+//Given a search term, search for tv shows that match the query. Returns the promise and array of shows (Obj): [show...]. Each obj should conatain id, name, simmary, image. If the show doesn't have an image, provide a default image. 
+async function getShowsByTerm(term) {
+  const response = await axios({
+    baseURL: TVMAZE_API_URL,
+    url: "search/shows",
+    method: "GET",
+    params: {
+      q: term,
+    },
+  });
+
+  return response.data.map(result => {
+    const show = result.show;
+    return {
+      id: show.id,
+      name: show.name,
+      summary: show.summary,
+      image: show.image ? show.image.large : MISSING_IMAGE_URL,
+    };
+  });
+}
+//Given a list of shows, create a markup in the DOM for each
+function populateShows(shows) {
+  $showsList.empty();
+
+  for (let show of shows) {
+    const $show = $(`
+        <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+           <div class="media">
+             <img src="${show.image}" alt="${show.name}" class="w-25 me-3">
+             <div class="media-body">
+               <h5 class="text-primary">${show.name}</h5>
+               <div><small>${show.summary}</small></div>
+               <button class="btn btn-outline-light btn-sm Show-getEpisodes">
+                 Episodes
+               </button>
+             </div>
+           </div>
+        </div>
+      `);
+
+    $showsList.append($show);
+  }
+}
+//Get shows from the API annd dsiplay them 
+async function searchForShowAndDisplay() {
+  const term = $("#searchForm-term").val();
+  const shows = await getShowsByTerm(term);
+
+  $episodesArea.hide();
+  populateShows(shows);
+}
+
+$searchForm.on("submit", async function (evt) {
+  evt.preventDefault();
+  await searchForShowAndDisplay();
+});
+//Given a show ID, get the API and return(promise) array of episodes (id, name, season, number)
+async function getEpisodesOfShow(id) {
+  const response = await axios({
+    baseURL: TVMAZE_API_URL,
+    url: `shows/${id}/episodes`,
+    method: "GET",
+  });
+
+  return response.data.map(e => ({
+    id: e.id,
+    name: e.name,
+    season: e.season,
+    number: e.number,
+  }));
+}
+//Given a list of eposides, create an assoicated markup in the DOM
+function populateEpisodes(episodes) {
+  $episodesList.empty();
+
+  for (let episode of episodes) {
+    const $item = $(
+      `<li>
+         ${episode.name}
+         (season ${episode.season}, episode ${episode.number})
+       </li>
+      `);
+
+    $episodesList.append($item);
+  }
+
+  $episodesArea.show();
+}
+//Handle the click on the episodes button to get and display episodes
+async function getEpisodesAndDisplay(evt) {
+  // here's one way to get the ID of the show: search "closest" ancestor
+  // with the class of .Show (which is put onto the enclosing div, which
+  // has the .data-show-id attribute).
+  const showId = $(evt.target).closest(".Show").data("show-id");
+
+  // here's another way to get the ID of the show: search "closest" ancestor
+  // that has an attribute of 'data-show-id'. This is called an "attribute
+  // selector", and it's part of CSS selectors worth learning.
+  // const showId = $(evt.target).closest("[data-show-id]").data("show-id");
+
+  const episodes = await getEpisodesOfShow(showId);
+  populateEpisodes(episodes);
+}
+
+$showsList.on("click", ".Show-getEpisodes", getEpisodesAndDisplay);
+
